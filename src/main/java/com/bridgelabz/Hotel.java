5b9aff6a12fd06259c7137d7d4c400b58354a16c
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-25 07:35:53 +0530

    [vinit]:welcome

---
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Hotel {
+    String hotelName;
+    int rates;
+
+    public Hotel(String hotelName,int rates)
+    {
+        this.hotelName=hotelName;
+        this.rates=rates;
+    }
+}
---
+++ src/test/java/com/bridgelabz/hotelreservationtest.java
@@ -0,0 +1,5 @@
+package com.bridgelabz;
+
+public class hotelreservationtest {
+    HotelReservation hotelReservation = new HotelReservation();
+}
